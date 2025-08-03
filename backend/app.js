eeff929855c029c9c821f901b82cac4ca638edf9
Daniil Baida <devbaida78@gmail.com>
2025-08-03 17:27:38 +0100

    create ErrorHandler and Create Error util

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ import express from "express";
 import { PORT } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
+app.use(errorHandler);
 
 app.use("/api/auth", authRouter);
 
--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,11 @@
+import { createError } from "../utils/createError";
+
 export const register = async (req, res, next) => {
+    const { nome, email, password } = req.body;
     try {
+        if (!nome || !email || !password) {
+            throw createError("Missing required fields", 400);
+        }
     } catch (error) {
         next(error);
     }
