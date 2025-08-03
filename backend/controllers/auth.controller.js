187d3db3e65ddef9be17fb57a82e8b2374329451
Daniil Baida <devbaida78@gmail.com>
2025-08-03 17:41:12 +0100

    created generateVerificationCode util

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import { createError } from "../utils/createError";
 import { User } from "../models/User";
+import { hashPassword } from "../utils/hashPassword";
 
 export const register = async (req, res, next) => {
-    const { nome, email, password } = req.body;
+    const { name, email, password } = req.body;
     try {
         if (!nome || !email || !password) {
             throw createError("Missing required fields", 400);
@@ -10,6 +11,12 @@ export const register = async (req, res, next) => {
 
         const existingUser = User.findOne({ email });
         if (existingUser) throw createError("User already exists", 409);
+
+        const createdUser = User.create({
+            name,
+            email,
+            password: hashPassword(password),
+        });
     } catch (error) {
         next(error);
     }
---
+++ backend/utils/generateVerificationCode.js
@@ -0,0 +1,16 @@
+import crypto from "crypto";
+
+//Generates a random code based on the provided characters and length
+export function generateVerificationCode(length = 6) {
+    const chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let code = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = crypto.randomInt(0, chars.length);
+        code += chars[randomIndex];
+    }
+    return {
+        token: code,
+        expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours in ms
+    };
+}
