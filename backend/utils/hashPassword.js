6af1d306d95559cc9a5a7efaaf99943be2e2b4da
Daniil Baida <devbaida78@gmail.com>
2025-08-03 21:26:36 +0100

    Simplify hashPassword function by returning the hashed password directly

--- backend/utils/hashPassword.js
+++ backend/utils/hashPassword.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcryptjs";
 
 export async function hashPassword(password) {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    return hashedPassword;
+    return await bcrypt.hash(password, 10);
 }
